ba49776a7a940bb26ee8f63cb38df792066c2f2f
Abhishith S D <imabhishith@gmail.com>
2025-11-29 20:30:09 +0530

    Your commit message here

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
+<<<<<<< HEAD
 const PORT = process.env.PORT || 3000;
+=======
+const PORT = process.env.PORT || 3001;
+>>>>>>> 05eefb6 (Your commit message here)
 
 app.use(cors());
 app.use(express.json());
